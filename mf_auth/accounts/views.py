31237e8c2b0c031736d11a0dde8012c5d4a679a8
yoona-ring <yoona@nutragroup.com>
2020-10-25 22:32:35 -0700

    routing

--- mf_auth/accounts/views.py
+++ mf_auth/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse #for routing
 # Create your views here.
+
+#to create view for test
+def home(request):
+    return HttpResponse('home')
+
+def product(request):
+    return HttpResponse('product')
