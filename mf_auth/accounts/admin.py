9b21cec8485831faf5b49029186c7346867682d8
yoona-ring <yoona@nutragroup.com>
2020-10-26 22:40:01 -0700

    make model and relationship

--- mf_auth/accounts/admin.py
+++ mf_auth/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import * # import models
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Tag)
+admin.site.register(Order)
